18ec356358c4661983833a2fc5baef0cd2ffffd8
Abby-kr <abby.a.otieno@gmail.com>
2023-09-06 21:14:11 +0300

    Resubmission of Lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,18 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-  const linksList = links.map((link) => {
-    return <a href={link} key={link} >{link}</a>
-  })
-  return <nav> {linksList} </nav>;
+  // const linksList = links.map((link) => {
+  //   return <a href={link} key={link} >{link}</a>
+  // })
+
+  return (
+    <nav> 
+    {links.map((link) => {
+    return <a key={link} href={`#${link}`} >{link}</a>
+    })}
+   </nav>
+  )
+  
 }
 
 export default NavBar;
